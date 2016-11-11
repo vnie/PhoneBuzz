dc987f7e690335f904f3458af0a68d11ae7d0f9d
Victor Nie <victor.nie@outlook.com>
2016-11-10 23:47:47 -0800

    Testing calling function

--- run.py
+++ run.py
@@ -1,34 +1,49 @@
 from flask import Flask, Response, request, abort, render_template
 from twilio import twiml
+from twilio.rest import TwilioRestClient
 from twilio.util import RequestValidator
 import form
 import os
 
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
+TWILIO_PHONE_NUMBER = os.environ.get('TWILIO_PHONE_NUM', None)
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
+URL = "https://damp-scrubland-52655.herokuapp.com"
+#URL = "localhost:5000"
 
 app = Flask(__name__)
-
-#@app.route("/", methods=['GET'])
-#def call():
-#	''' Handles incoming calls '''
-#	validator = RequestValidator(TWILIO_AUTH_TOKEN)
-#	signature = request.headers.get('X-Twilio-Signature', '')
-#	
-#	if not validator.validate(request.url, request.form, signature):
-#		return abort(403)
-#
-#	resp = twiml.Response()
-#	with resp.gather(timeout=5, action="/handle-input", finishOnKey="#", method="POST") as g:
-#		g.say("Hi, please enter your FizzBuzz number and then press pound")
-#	
-#	return str(resp)
-
+app.config.update(SECRET_KEY=SECRET_KEY)
 
 @app.route("/", methods=['GET', 'POST'])
 def call():
-	form = PhoneForm()
+	phone_form = form.PhoneForm()
 
-	return render_template('index.html', form=form)
+	if phone_form.submit():
+		phone_num = phone_form.phone_num.data
+		# Make call
+		client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+		call = client.calls.create(to=phone_num, 
+								   from_=TWILIO_PHONE_NUMBER, 
+								   url=URL + "/call")
+		
+	return render_template('index.html', form=phone_form)
+
+
+@app.route("/call", methods=['GET'])
+def get_input():
+	''' Handles incoming calls '''
+	validator = RequestValidator(TWILIO_AUTH_TOKEN)
+	signature = request.headers.get('X-Twilio-Signature', '')
+	
+	if not validator.validate(request.url, request.form, signature):
+		return abort(403)
+
+	resp = twiml.Response()
+	with resp.gather(timeout=5, action="/handle-input", finishOnKey="#", method="POST") as g:
+		g.say("Hi, please enter your FizzBuzz number and then press pound")
+	
+	return str(resp)
 
 
 @app.route("/handle-input", methods=['POST'])
