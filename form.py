528f6e93b0cde455868e38e315eb278d47f19dfa
Victor Nie <victor.nie@outlook.com>
2016-11-10 23:47:47 -0800

    Testing calling function

--- form.py
+++ form.py
@@ -5,3 +5,9 @@ class PhoneForm(Form):
 	phone_num_validator = [validators.Required(), validators.regexp(u'[0-9]+')]
 	phone_num = TextField('Phone # to call', phone_num_validator)
 
+	def submit(self):
+		# Expects 9/10 digit phone numbers i.e
+		# 555-555-555 or 1-555-555-555
+		if self.phone_num.data != None and (len(self.phone_num.data) == 9 or len(self.phone_num.data) == 10):
+			return True
+		return False
